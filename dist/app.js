dc5ddc3972f48518cc186fdb8cf3d2807bcf4b3b
Holografick <ayzhbd@gmail.com>
2018-12-04 23:06:44 +0600

    root app component with quizzing and browsing states

--- dist/app.js
+++ dist/app.js
@@ -1,5 +1,46 @@
-ReactDOM.render(React.createElement(
-  "h1",
-  null,
-  "hello!"
-), document.getElementById("react_container"));
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var App = function (_React$Component) {
+    _inherits(App, _React$Component);
+
+    function App(props) {
+        _classCallCheck(this, App);
+
+        var _this = _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).call(this, props));
+
+        _this.state = {
+            status: 'browsing',
+            quiz: null
+        };
+        return _this;
+    }
+
+    _createClass(App, [{
+        key: 'render',
+        value: function render() {
+            if (this.state.status === 'browsing') {
+                return React.createElement(
+                    'div',
+                    null,
+                    'Select quiz you wish to take'
+                );
+            } else if (this.state.status === 'quizzing') {
+                return React.createElement(
+                    'div',
+                    null,
+                    'Quizzing'
+                );
+            }
+        }
+    }]);
+
+    return App;
+}(React.Component);
+
+ReactDOM.render(React.createElement(App, null), document.getElementById("react_container"));
--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
-ReactDOM.render(<h1>hello!</h1>, document.getElementById("react_container"))
+class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            status: 'browsing',
+            quiz: null
+        }
+    }
+    render(){
+        if(this.state.status === 'browsing'){
+            return (
+                <div>Select quiz you wish to take</div>            
+            )
+        } else if(this.state.status === 'quizzing'){
+            return (
+                <div>Quizzing</div>
+            )
+        }
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById("react_container"))
